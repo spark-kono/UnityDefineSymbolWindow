6f72b405ae954b617531a37859a90b8977c796bc
MasahiroKouno <ennoiamayall@gmail.com>
2021-02-17 01:04:57 +0900

    DefineSymbolWindow ver.1.0.0 Release!!

--- DefineSymbolWindow/Assets/DefineSymbolWindow/Scripts/Editor/DefineSymbolWindow.cs
+++ DefineSymbolWindow/Assets/DefineSymbolWindow/Scripts/Editor/DefineSymbolWindow.cs
@@ -1,18 +1,17 @@
-﻿/* DefineSymbolWindow.cs
- * SparkCreative.inc      
- * Update : 2021/02/14  Masahiro Kono
- * 
- * 
- * 
- * 
+﻿/* DefineSymbolWindow.cs      
+ * Create : 2021/02/14  Masahiro Kono
  */
 using System;
+using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEditor;
 
 public class DefineSymbolWindow : EditorWindow
 {
+    private static readonly string SAVE_DATA_DIRECTORY = "/DefineSymbolWindow/SaveData/";
+    private static readonly string SAVE_DATA_PATH = SAVE_DATA_DIRECTORY + "SaveData.json";
     private static SaveData saveData = new SaveData();
     private Vector2 buildTargetGroupDataScrollPosition = Vector2.zero;
     private Vector2 defineSymbolDataScrollPosition = Vector2.zero;
@@ -25,6 +24,8 @@ public class DefineSymbolWindow : EditorWindow
 
         public void DefaultSetting()
         {
+            buildTargetGroupDataList.Clear();
+
             foreach (BuildTargetGroup value in Enum.GetValues(typeof(BuildTargetGroup)))
             {
                 if(value == BuildTargetGroup.Unknown)
@@ -52,6 +53,7 @@ public class DefineSymbolWindow : EditorWindow
                 buildTargetGroupDataList.Add(addData);
             }
 
+            defineSymbolDataList.Clear();
             AddDefineSymbol();
         }
 
@@ -94,11 +96,6 @@ public class DefineSymbolWindow : EditorWindow
     {
         var window = EditorWindow.GetWindow<DefineSymbolWindow>("DefineSymbol");
         window.maxSize = new Vector2(800, 600);
-    }
-    
-    private void Start()
-    {
-        
     }
 
     private void OnEnable()
@@ -151,9 +148,17 @@ public class DefineSymbolWindow : EditorWindow
                     saveData.AddDefineSymbol();
                 }
 
-                if (GUILayout.Button("Setting", EditorStyles.toolbarButton, GUILayout.Width(80)))
+                if (GUILayout.Button("Set", EditorStyles.toolbarButton, GUILayout.Width(50)))
                 {
                     Setting();
+                }
+
+                if (GUILayout.Button("Reset", EditorStyles.toolbarButton, GUILayout.Width(50)))
+                {
+                    if (EditorUtility.DisplayDialog("DefineSymbolSetting", "Do you want to reset and return to the default settings?", "Yes", "No"))
+                    {
+                        DefalutSetting();
+                    }
                 }
             }
             GUILayout.EndHorizontal();
@@ -192,19 +197,94 @@ public class DefineSymbolWindow : EditorWindow
 
     private void Setting()
     {
-        if(EditorUtility.DisplayDialog("DefineSymbolSetting", "Do you want to reflect the define symbol settings?", "Yes", "No"))
+        if (!EditorUtility.DisplayDialog("DefineSymbolSetting", "Do you want to reflect the define symbol settings?", "Yes", "No"))
         {
-            Save();
+            return;
+        }
+
+        Save();
+
+        foreach (var buildTargetGroupData in saveData.buildTargetGroupDataList)
+        {
+            if (!buildTargetGroupData.flag)
+            {
+                continue;
+            }
+
+            var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroupData.buildTargetGroup).Split(';').ToList();
+            symbols.Clear();
+
+            foreach (var defineSymbolData in saveData.defineSymbolDataList)
+            {
+                if (!defineSymbolData.flag)
+                {
+                    continue;
+                }
+
+                if(string.IsNullOrEmpty(defineSymbolData.defineSymbol))
+                {
+                    continue;
+                }
+
+                symbols.Add(defineSymbolData.defineSymbol);
+            }
+
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroupData.buildTargetGroup, string.Join(";", symbols));
         }
     }
 
+    
+
     private void Save()
     {
+        var directoryPath = Application.dataPath + SAVE_DATA_DIRECTORY;
 
+        if (!Directory.Exists(directoryPath))
+        {
+            Directory.CreateDirectory(directoryPath);
+        }
+
+        var json = JsonUtility.ToJson(saveData);
+        var savePath = Application.dataPath + SAVE_DATA_PATH;
+        StreamWriter writer = new StreamWriter(savePath, false);
+        writer.WriteLine(json);
+        writer.Flush();
+        writer.Close();
+        AssetDatabase.Refresh();
     }
 
     private void Load()
     {
+        if (File.Exists(Application.dataPath + SAVE_DATA_PATH))
+        {
+            FileStream stream = File.Open(Application.dataPath + SAVE_DATA_PATH, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+
+            var json = reader.ReadToEnd();
+            reader.Close();
+            stream.Close();
+
+            var loadData = JsonUtility.FromJson<SaveData>(json);
+
+            // デフォルト設定にしてBuildTargetGroupにプラットフォーム追加更新が無いか確認・シンボルはそのまま読み込んで代入
+            saveData.DefaultSetting();
+            saveData.defineSymbolDataList = loadData.defineSymbolDataList;
+
+            // プラットフォームは増えることがあっても基本減ることは無いと思うのでデフォルト設定読み込んだデータにロードデータのフラグを順番に設定
+            for (int i = 0; i < loadData.buildTargetGroupDataList.Count; i++)
+            {
+                saveData.buildTargetGroupDataList[i].flag = loadData.buildTargetGroupDataList[i].flag;
+            }
+
+            return;
+        }
+
+        DefalutSetting();
+    }
+
+    private void DefalutSetting()
+    {
         saveData.DefaultSetting();
+        Save();
     }
 }
